cdc6975a5b7801a74cc4f577ceb38da9706172d3
tetsuromori <tetsuromori@gmail.com>
2020-10-08 16:07:25 +0900

    initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,42 @@
+import 'dart:convert' show json;
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import './models/image_model.dart';
+import './widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('http://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text('Let\'s see some images!'),
+        ),
+      ),
+    );
+  }
+}
